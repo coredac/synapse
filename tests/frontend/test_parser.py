b9e2576bf3dc1d09587496f0fb85e43f342541df
ShangkunLI <shangkun.li@connect.ust.hk>
2026-06-21 15:00:30 +0800

    add the ast dump as comment

--- tests/frontend/test_parser.py
+++ tests/frontend/test_parser.py
@@ -25,6 +25,88 @@ def test_parse_function_captures_gemm_source():
     assert [arg.arg for arg in fn.args.args] == ["A", "B", "C"]
 
 
+# Full dump of dump_ast(gemm):
+#
+# Module(
+#   body=[
+#     FunctionDef(
+#       name='gemm',
+#       args=arguments(
+#         posonlyargs=[],
+#         args=[
+#           arg(arg='A'),
+#           arg(arg='B'),
+#           arg(arg='C')],
+#         kwonlyargs=[],
+#         kw_defaults=[],
+#         defaults=[]),
+#       body=[
+#         For(
+#           target=Name(id='i', ctx=Store()),
+#           iter=Call(
+#             func=Name(id='range', ctx=Load()),
+#             args=[
+#               Constant(value=128)],
+#             keywords=[]),
+#           body=[
+#             For(
+#               target=Name(id='j', ctx=Store()),
+#               iter=Call(
+#                 func=Name(id='range', ctx=Load()),
+#                 args=[
+#                   Constant(value=128)],
+#                 keywords=[]),
+#               body=[
+#                 Assign(
+#                   targets=[
+#                     Name(id='acc', ctx=Store())],
+#                   value=Constant(value=0.0)),
+#                 For(
+#                   target=Name(id='k', ctx=Store()),
+#                   iter=Call(
+#                     func=Name(id='range', ctx=Load()),
+#                     args=[
+#                       Constant(value=128)],
+#                     keywords=[]),
+#                   body=[
+#                     AugAssign(
+#                       target=Name(id='acc', ctx=Store()),
+#                       op=Add(),
+#                       value=BinOp(
+#                         left=Subscript(
+#                           value=Name(id='A', ctx=Load()),
+#                           slice=Tuple(
+#                             elts=[
+#                               Name(id='i', ctx=Load()),
+#                               Name(id='k', ctx=Load())],
+#                             ctx=Load()),
+#                           ctx=Load()),
+#                         op=Mult(),
+#                         right=Subscript(
+#                           value=Name(id='B', ctx=Load()),
+#                           slice=Tuple(
+#                             elts=[
+#                               Name(id='k', ctx=Load()),
+#                               Name(id='j', ctx=Load())],
+#                             ctx=Load()),
+#                           ctx=Load())))],
+#                   orelse=[]),
+#                 Assign(
+#                   targets=[
+#                     Subscript(
+#                       value=Name(id='C', ctx=Load()),
+#                       slice=Tuple(
+#                         elts=[
+#                           Name(id='i', ctx=Load()),
+#                           Name(id='j', ctx=Load())],
+#                         ctx=Load()),
+#                       ctx=Store())],
+#                   value=Name(id='acc', ctx=Load()))],
+#               orelse=[])],
+#           orelse=[])],
+#       decorator_list=[],
+#       type_params=[])],
+#   type_ignores=[])
 def test_dump_ast_contains_gemm_nodes():
     dumped = dump_ast(gemm)
 
