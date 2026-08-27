9e8f5dea9174dce37535cd9c0abaf044448d7b8f
ShangkunLI <shangkun.li@connect.ust.hk>
2026-08-27 20:55:31 +0800

    Add mac operation in language

--- python/synapse/language/__init__.py
+++ python/synapse/language/__init__.py
@@ -1,9 +1,17 @@
 """Public Synapse language API."""
 
 from .spatial import TileArray
-from .tile_array_program import TileArrayScalarType, add, constant
+from .tile_array_program import TileArrayScalarType, add, constant, mac
 
 i32 = TileArrayScalarType.I32
 f32 = TileArrayScalarType.F32
 
-__all__ = ["TileArray", "TileArrayScalarType", "add", "constant", "f32", "i32"]
+__all__ = [
+    "TileArray",
+    "TileArrayScalarType",
+    "add",
+    "constant",
+    "f32",
+    "i32",
+    "mac",
+]
